c3402d98f5e83ca2569b5348d682d43d7de9b48b
Ren Simmons <rsimmo4@gmail.com>
2019-03-29 15:47:38 -0500

    fix var memory storage issue

--- OchsnerSensorMega.ino
+++ OchsnerSensorMega.ino
@@ -3,10 +3,10 @@
 #include <PubSubClient.h>
 
 // Update these with values suitable for your hardware/network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 97);
-IPAddress server(192, 168, 1, 77);
 
+// Analog Readings from sensor:
 // ACTIVE: 0-450
 // PRESENT: 451-900
 // IDLE: > 900
@@ -15,32 +15,32 @@ IPAddress server(192, 168, 1, 77);
 #define PRESENT_STATE 1
 #define IDLE_STATE 2
 
-#define STATE_CHANGE_BUFFER_SECONDS 1
+#define STATE_CHANGE_BUFFER_SECONDS 2
 #define ACTIVE_MAX_INCHES 24
 #define PRESENT_MAX_INCHES 48
 
 // Station Pins
-const int L1_sensorPin = A0;
+const int L1_sensorPin = 54;
 const int L1_activePin = 22;
 const int L1_presentPin = 23;
 
-const int L2_sensorPin = A1;
+const int L2_sensorPin = 55;
 const int L2_activePin = 24;
 const int L2_presentPin = 25;
 
-const int L3_sensorPin = A2;
+const int L3_sensorPin = 56;
 const int L3_activePin = 26;
 const int L3_presentPin = 27;
 
-const int R1_sensorPin = A3;
+const int R1_sensorPin = 57;
 const int R1_activePin = 28;
 const int R1_presentPin = 29;
 
-const int R2_sensorPin = A4;
+const int R2_sensorPin = 58;
 const int R2_activePin = 30;
 const int R2_presentPin = 31;
 
-const int R3_sensorPin = A5;
+const int R3_sensorPin = 59;
 const int R3_activePin = 32;
 const int R3_presentPin = 33;
 
@@ -56,10 +56,10 @@ EthernetClient net;
 PubSubClient client(net);
 
 // String brokerIp = "192.168.1.77";
-const char* mqtt_server = "broker.mqtt-dashboard.co";
+const char* mqtt_server = "192.168.1.69";
 
-const String stations[6] = {"L1", "L2", "L3", "R1", "R2", "R3"};
-const String states[3] = {"ACTIVE", "PRESENT", "IDLE"};
+const char stations[6][10] = {"L1", "L2", "L3", "R1", "R2", "R3"};
+const char states[3][10] = {"ACTIVE", "PRESENT", "IDLE"};
 int currentStates[6] = {L1_currentState, L2_currentState, L3_currentState, R1_currentState, R2_currentState, R3_currentState};
 
 const int sensorPins[6] = {L1_sensorPin, L2_sensorPin, L3_sensorPin, R1_sensorPin, R2_sensorPin, R3_sensorPin};
@@ -75,11 +75,13 @@ void reconnect() {
   while (!client.connected()) {
         Serial.print("Attempting MQTT connection...");
         // Attempt to connect
-        if (client.connect("arduino")) {
+        if (client.connect("magicSurfaceClient")) {
             Serial.println("connected");
             // Once connected, publish an announcement...
             client.publish("magicSurface", "hello world");
-            client.subscribe("magicSurface");
+            for (int i = 0; i < 6; i++) {
+              client.subscribe(stations[i]);
+            }
             // client.unsubscribe("magicSurface");
         } else {
             Serial.print("failed, rc=");
@@ -121,57 +123,65 @@ void setup() {
 
 void loop() {
   if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
+      reconnect();
+  }
+  client.loop();
 
   for (int i = 0; i < 6; i++) {
     stateMachine(i);
   }
-
-  // publish a message roughly every second.
-  //  if (millis() - lastMqttMillis > 1000) {
-  //    lastMqttMillis = millis();
-  //    client.publish("/hello", "world");
-  //  }
-  
   delay(100);
 }
 
+int lastTempState[6];
 void stateMachine (int pos) {
   int tempState;
   long analogDistance, inches;
-  volatile static int lastTempState = IDLE_STATE;
   volatile static unsigned long lastStateChangeTime = 0;
-  unsigned long stateTime = millis();
-  
+
   analogDistance = analogRead(sensorPins[pos]);
   inches = analogToInches(analogDistance);
-  
+
   tempState = getState(inches);
 
-  if (tempState != lastTempState && lastTempState == currentStates[pos]) {
+  if (tempState != lastTempState[pos] && lastTempState[pos] == currentStates[pos]) {
     lastStateChangeTime = millis();
   }
   
-  if (tempState != currentStates[pos] && stateTime - lastStateChangeTime > STATE_CHANGE_BUFFER_SECONDS * 1000) {
-    lastStateChangeTime = stateTime;
+  if (
+    tempState != currentStates[pos] &&
+    tempState == lastTempState[pos] &&
+    millis() - lastStateChangeTime > STATE_CHANGE_BUFFER_SECONDS * 1000
+  ) {
+#if DEBUG == 1    
+    Serial.print("Sensor State Changed: ");
+    Serial.println(stations[pos]);
+    Serial.print("Last State: ");
+    Serial.println(currentStates[pos]);
+    Serial.print("New State: ");
+    Serial.println(tempState);
+#endif
+    lastStateChangeTime = millis();
     currentStates[pos] = tempState;
-    client.publish(stations[pos], states[pos], 1883);
+    
+    client.publish(stations[pos], states[currentStates[pos]]);
   }
   if (currentStates[pos] == IDLE_STATE) {
-    Serial.println("" + stations[pos] + " IDLE");
+    // Serial.print(stations[pos]);
+    // Serial.println(states[pos]);
     setIdle(pos);
   }
   if (currentStates[pos] == PRESENT_STATE) {
-    Serial.println("" + stations[pos] + " PRESENT");
+    // Serial.print(stations[pos]);
+    // Serial.println(states[pos]);
     setPresent(pos);
   }
   if (currentStates[pos] == ACTIVE_STATE) {
-    Serial.println("" + stations[pos] + " ACTIVE");
+    // Serial.print(stations[pos]);
+    // Serial.println(states[pos]);
     setActive(pos);
   }
-  lastTempState = tempState;
+  lastTempState[pos] = tempState;
 }
 
 void setActive (int pos) {
@@ -194,7 +204,7 @@ long analogToInches(long analogDistance) {
 #if DEBUG == 1  
   Serial.println(analogDistance);
 #endif
-  return map(analogDistance, 50, 1023, 10, 48);
+  return map(analogDistance, 50, 1023, 10, 55);
 }
 
 int getState(long inches) {
